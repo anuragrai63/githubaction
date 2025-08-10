091a2f0279a806a3ad7ed170b2f65a23a307a605
Anurag Rai <anuragrai@Anurags-MacBook-Air.local>
2025-08-10 11:46:56 +0530

    eks_upd

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,20 +9,7 @@ terraform {
   backend "s3" {}
 }
 
-resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
-  type  = "String"
-  value = "barr"
-}
-
-resource "aws_instance" "vs-test" {
-    ami = "ami-00beae93a2d981137"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0d0e3828e26eb661c"
-    availability_zone = "us-east-1d"
-    tags = {
-      "Name" = "VS-instance"
-    }
-  
+provider "aws" {
+  region = "us-east-1"
 }
 
